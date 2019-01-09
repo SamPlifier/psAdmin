a0f71df664aca94d3c9179fb238dd1d331a6315c
SamDC <sam.davis.castro@gmail.com>
2019-01-08 21:45:54 -0500

    registered flux store with dispatcher, added 2 methods to store

--- src/stores/authorStore.js
+++ src/stores/authorStore.js
@@ -4,9 +4,13 @@ var Dispatcher = require('../dispatcher/appDispatcher');
 var ActionTypes = require('../constants/actionTypes');
 var EventEmitter = require('events').EventEmitter;
 var assign = require('object-assign');
+var _ = require('lodash');
 var CHANGE_EVENT = 'change';
 
-// take an empty new object, extend it to use event emitter capabiliti
+var _authors = [];//private variable- not exported out of module so only way to change data in store is with methods in AuthorStore
+
+// take an empty new object, extend it to use event emitter capabilities
+//AuthorStore is the public API for our store
 var AuthorStore = assign({}, EventEmitter.prototype, {
     // addChangeListener, removeChangeListener & emitChange give store ability to interact with React components
     addChangeListener: function(callback) {
@@ -17,5 +21,21 @@ var AuthorStore = assign({}, EventEmitter.prototype, {
     },
     emitChange: function() {
         this.emit('change');
+    },
+    getAllAuthors: function() {
+        return _authors;
+    },
+    getAuthorById: function(id) {
+        return _.find(_authors, {id : id});
     }
 });
+
+Dispatcher.register(function(action) {
+    switch(action.actionType) {
+        case ActionTypes.CREATE_AUTHOR:
+            _authors.push(action.author);
+            AuthorStore.emitChange();
+    }
+});
+
+module.exports = AuthorStore;
